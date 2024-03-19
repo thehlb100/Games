2b77488bb72093d15832fb4b3cb31d0d9d448b5a
thehlb100 <156230000+thehlb100@users.noreply.github.com>
2024-03-19 17:37:28 +0100

    Update Ahorcado.py

--- Ahorcado.py
+++ Ahorcado.py
@@ -1,41 +1,61 @@
 from random import randint
 import math
 
-#Elección de palabras a partir de archivo de texto
+def ahorcado():
+ #Elección de palabras a partir de archivo de texto
 
-archivoPalabras = open("ordenador.txt", "r")
-palabras = archivoPalabras.read()
-listaPalabras = palabras.split("\n")
-longitudListaPalabras = len(listaPalabras)
-palabraSeleccionada = randint(0, longitudListaPalabras-1)
-intentos = 10
+ #Temática ordenador
+ archivoPalabras = open("ordenador.txt", "r")
+ palabras = archivoPalabras.read()
+ listaPalabras = palabras.split("\n")
+ longitudListaPalabras = len(listaPalabras)
+ palabraSeleccionada = randint(0, longitudListaPalabras-1)
 
-#Juego
-def pregunta():
- print ('Que palabra escoges?')
- respuesta = str(input())
- if respuesta == (listaPalabras[palabraSeleccionada]):
+ #Temática frutas y verduras
+ archivoPalabras1 = open('frutas.txt',"r")
+ palabras1 = archivoPalabras1.read()
+ listaPalabras1= palabras1.split("\n")
+ longitudListaPalabras1= len(listaPalabras1)
+ palabraSeleccionada1= randint(0,longitudListaPalabras1-1)
+
+ #Inicio
+ print ("Bienvenido al juego del ahorcado!!")
+ print ("Tienes 10 intentos para adivinar una palabra")
+ print ('Qué temática desesas escoger')
+ print ('Partes del ordenador (1)')
+ print ('Frutas y verduras (2)')
+
+ #Elección de la temática
+ NivelElegido = int(input())
+
+
+ #Juego
+ if NivelElegido == 1:
+  print ('Que palabra escoges?')
+  respuesta = str(input())
+  if respuesta == (listaPalabras[palabraSeleccionada]):
     print ("Has ganado")
 
- else:
+  else:
    print ('No es correcto')
-   intentos-1
-   print (f"El número de intentos restantes es de {intentos}")
+   print (f'La respuesta correcta era {longitudListaPalabras[palabraSeleccionada]}')
+
+   
+
+ if NivelElegido==2:
+  print ('Que palabra escoges?')
+  respuesta = str(input())
+  if respuesta == (listaPalabras1[palabraSeleccionada1]):
+    print('Has ganado!')
   
-   pregunta()
- if intentos == 0:
-   print ("Has perdido")
-   print (f'La respuesta correcta era {listaPalabras[palabraSeleccionada]}')
+  else:
+    print('La palabra no es correcta')
+    print (f'La respuesta correcta era {longitudListaPalabras1[palabraSeleccionada1]}')
 
-#Inicio
-print ("Bienvenido al juego del ahorcado!!")
-print ("Tienes 10 intentos para adivinar una palabra")
-print ("La palabra está realacionada con la informática")
-print ("Que comience el juego!")
-pregunta()
+  
 
+ahorcado()
 
-print('Game made by TheHlb100')
 
 
 
